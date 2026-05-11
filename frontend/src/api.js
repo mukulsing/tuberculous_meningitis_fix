6daf3150725f182513feefe83e63ce35ff5351ee
mukulsing <138805193+mukulsing@users.noreply.github.com>
2026-05-12 04:05:56 +0530

    Update api.js

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "https://your-backend-url.onrender.com",
+  baseURL: "https://tuberculous-meningitis-fix.onrender.com/",
 });
 
 export default API;
