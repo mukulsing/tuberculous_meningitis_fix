9eeaecb1df84030f66fa81adba21ff9b2b383291
mukulsing <138805193+mukulsing@users.noreply.github.com>
2026-05-12 11:52:24 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -6,5 +6,11 @@ app = Flask(__name__)
 CORS(app)
 
 if __name__ == '__main__':
+    import os
+
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+
+    app.run(
+        host='0.0.0.0',
+        port=port
+    )
