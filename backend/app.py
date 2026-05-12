c8944b567a83c39a4d79017ea9f7524a995ccdec
mukulsing <138805193+mukulsing@users.noreply.github.com>
2026-05-12 12:14:59 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -14,3 +14,8 @@ if __name__ == '__main__':
         host='0.0.0.0',
         port=port
     )
+@app.route('/')
+def home():
+    return jsonify({
+        "message": "TBM Backend API Running Successfully"
+    })
